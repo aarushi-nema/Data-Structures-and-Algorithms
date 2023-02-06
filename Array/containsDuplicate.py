97d74d21da4f472087380efc5ecda138c15040b6
aarushi-nema <aarushi.nema@gmail.com>
2023-02-07 00:12:44 +0800

    [Arrays] Contains duplicate

---
+++ Array/containsDuplicate.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+
+def containsDuplicate(nums):
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
